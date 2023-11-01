40b3a18db623adf0c951589472e16fa6d7910ec4
DilshanSenewirathne <dilshansenevi99@gmail.com>
2023-11-01 17:43:25 +0530

    feat: service provider subscription

--- frontend/src/components/TicketsPage/TicketCarousel.jsx
+++ frontend/src/components/TicketsPage/TicketCarousel.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { Carousel } from 'react-bootstrap'
 import image1 from '../../assets/images/tickets/CT-NEW-EVENT.jpg'
 import image2 from '../../assets/images/tickets/event-new-api-hamuwemuda.jpg'
@@ -8,7 +8,29 @@ import image5 from '../../assets/images/tickets/legends-new-event-banner.jpg'
 import image6 from '../../assets/images/tickets/MA-NOWANA-MAMA11.png'
 import image7 from '../../assets/images/tickets/Marians-teaser.jpg'
 import image8 from '../../assets/images/tickets/sankranthi-event-new.jpg'
+import axios from 'axios'
 const TicketCarousel = () => {
+  const [ticketImages, setTicketImages] = useState([]);
+
+  useEffect(() => {
+    // Make a GET request when the component mounts
+    axios
+      .get('/api/tickets/getTrendingTickets')
+      .then((response) => {
+        // Update the state with the fetched data
+        setTicketImages(response.data.getTicketResponse);
+      })
+      .catch((error) => {
+        console.error('Error fetching tickets:', error);
+      });
+  }, []);
+
+  const updatedImages = ticketImages.map(item => {
+    return { eventposter: `../../src/assets/images/uploads/${item.eventposter}` };
+  });
+
+  console.log(updatedImages);
+
   const images = [
     image1,
     image2,
@@ -19,7 +41,9 @@ const TicketCarousel = () => {
     image7,
     image8,
   ]
+
   return (
+    
     <Carousel fade>
       {images.map((image, index) => (
         <Carousel.Item key={index}>
